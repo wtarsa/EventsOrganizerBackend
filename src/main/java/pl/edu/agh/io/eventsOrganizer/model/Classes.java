c389d5c31ed0a34a4b680f1c2ce16f5c9a721c99
Przemysław Lechowicz <lechowiczprzemyslaw@gmail.com>
2021-03-23 19:42:23 +0100

    Separated date field in Classes into startTime&endTime

--- src/main/java/pl/edu/agh/io/eventsOrganizer/model/Classes.java
+++ src/main/java/pl/edu/agh/io/eventsOrganizer/model/Classes.java
@@ -2,12 +2,7 @@ package pl.edu.agh.io.eventsOrganizer.model;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -15,12 +10,20 @@ import java.time.LocalDateTime;
 @Data
 public class Classes {
 
-    public Classes(){
+    public Classes() {
     }
 
     public Classes(String name, String classroom) {
         this.name = name;
-        this.date = LocalDateTime.now(); // do zmiany koniecznie! ale teraz nie chce mi sie pisac parsera
+        this.startTime = LocalDateTime.now(); // do zmiany koniecznie! ale teraz nie chce mi sie pisac parsera
+        this.endTime = LocalDateTime.now(); // do zmiany koniecznie! ale teraz nie chce mi sie pisac parsera
+        this.classroom = classroom;
+    }
+
+    public Classes(String name, String classroom, LocalDateTime startTime, LocalDateTime endTime) {
+        this.name = name;
+        this.startTime = startTime;
+        this.endTime = endTime;
         this.classroom = classroom;
     }
 
@@ -29,9 +32,8 @@ public class Classes {
     private long id;
 
     private String name;
-
-    private LocalDateTime date;
-
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
     private String classroom;
 
     @ManyToOne
